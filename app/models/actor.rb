a42ccf54c80903f83c167b265745123467afd43f
ishwo6375 <ishaan.prazapati6375@gmail.com>
2021-09-30 10:04:13 -0400

    defined relation to models

--- app/models/actor.rb
+++ app/models/actor.rb
@@ -1,2 +1,5 @@
 class Actor < ApplicationRecord
+ has_many :contracts, dependent: :destroy
+ has_many :movies, through: :contracts
+ 
 end
--- app/models/movie.rb
+++ app/models/movie.rb
@@ -1,3 +1,6 @@
 class Movie < ApplicationRecord
   belongs_to :director
+    has_many :reviews, dependent: :destroy
+    has_many :contracts, dependent: :destroy
+    has_many :actors, through: :contracts
 end
