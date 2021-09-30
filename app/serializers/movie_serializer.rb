c2f5b14238f13ae0ec8b47ae1e0219838d9dbd41
ishwo6375 <ishaan.prazapati6375@gmail.com>
2021-09-30 11:54:45 -0400

    app deployed to heroku

--- app/serializers/movie_serializer.rb
+++ app/serializers/movie_serializer.rb
@@ -1,3 +1,3 @@
 class MovieSerializer < ActiveModel::Serializer
-   attributes :id, :title, :description, :language, :runtime, :rating, :image, :video_url, :genre, :director 
+   attributes :id, :title, :description, :language, :runtime, :rating, :image, :video_url, :genre, :reviews, :director 
 end
